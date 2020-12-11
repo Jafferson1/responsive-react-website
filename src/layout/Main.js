3b4dd24b1b0d58d9ab5f917dc0f2b73ab9ad8e82
jafferson <jaffersonaraujo@gmail.com>
2020-12-11 20:52:24 -0300

    adding sections navigation and classesNames

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,16 +1,18 @@
 import React from "react";
+import Conclusion from "../components/sections/Conclusion";
 import Hero from "../components/sections/Hero";
+import HowItWorks from "../components/sections/HowItWorks";
 import Introduction from "../components/sections/Introduction";
+import WhyYouShould from "../components/sections/WhyYouShould";
 
 const Main = () => {
   return (
     <>
-      <div className="hero">
-        <Hero />
-      </div>
-      <div className="introduction">
-        <Introduction />
-      </div>
+      <Hero className="hero" />
+      <Introduction section="introduction" className="introduction wave" />
+      <HowItWorks section="how-it-works" className="how-it-works" />
+      <WhyYouShould section="why-you-should" className="why-you-should wave" />
+      <Conclusion section="conclusion" className="conclusion" />
     </>
   );
 };
